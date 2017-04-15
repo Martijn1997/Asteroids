4c4a3620dc34efd7f0525c7557f155c30465b608
flortheuns <flor.theuns@hotmail.com>
2017-04-15 14:41:20 +0200

    Klaar

--- OGP1617-Asteroids/tests/asteroids/tests/WorldTest.java
+++ OGP1617-Asteroids/tests/asteroids/tests/WorldTest.java
@@ -19,8 +19,8 @@ public class WorldTest {
 	private static final double EPSILON = 0.0001;
 	
 	private static World world1, world2;
-	private static Ship ship1, ship2, ship3, ship4, ship5, ship6, ship7, ship8;
-	private static Bullet bullet1, bullet2, bullet3;
+	private static Ship ship1, ship2, ship3, ship4, ship5, ship6, ship7, ship8, ship9, ship10;
+	private static Bullet bullet1, bullet2, bullet3, bullet4;
 	
 	@Before
 	public void setUpMutableFixture(){
@@ -34,9 +34,12 @@ public class WorldTest {
 		ship6 = new Ship(400,200,0,10,-100,100,0);
 		ship7 = new Ship(440,2000,0,400,0,0,0);
 		ship8 = new Ship(1000,200,0,10,-100,0,0);
+		ship9 = new Ship(1000,200,0,10,100,0,0);
+		ship10 = new Ship(1400,200,0,10,-100,0,0);
 		bullet1 = new Bullet(500, 400, 2, 0, 0, 1);
 		bullet2 = new Bullet(52, 700, 2, -100, 0, 1);
 		bullet3 = new Bullet(1000, 1000, 2, 50,70, 1);
+		bullet4 = new Bullet(1400, 200, 2, -100,0, 1);
 	}
 	
 	@Test
@@ -185,7 +188,7 @@ public class WorldTest {
 	}
 	
 	@Test
-	public final void evolve_NoCollision(){
+	public final void evolve_Thrust(){
 		world1.addWorldObject(ship1);
 		world1.addWorldObject(ship5);
 		world1.addWorldObject(ship7);
@@ -197,12 +200,34 @@ public class WorldTest {
 		ship7.thrustOn();
 		world1.evolve(1, null);
 		assertEquals(oldPos1.getXComponent(), ship1.getPosition().getXComponent(), EPSILON);
-		assertEquals(newPos2.getXComponent(), ship5.getPosition().getXComponent(), EPSILON);
-		assertEquals(newPos3.getXComponent(), bullet3.getPosition().getXComponent(), EPSILON);
 		assertEquals(oldPos1.getYComponent(), ship1.getPosition().getYComponent(), EPSILON);
+		assertEquals(newPos2.getXComponent(), ship5.getPosition().getXComponent(), EPSILON);
 		assertEquals(newPos2.getYComponent(), ship5.getPosition().getYComponent(), EPSILON);
+		assertEquals(newPos3.getXComponent(), bullet3.getPosition().getXComponent(), EPSILON);
 		assertEquals(newPos3.getYComponent(), bullet3.getPosition().getYComponent(), EPSILON);
 		assertEquals(xVel, ship7.getXVelocity(), EPSILON);
+	}
+	
+	@Test
+	public final void evolve_1Collision(){
+		world1.addWorldObject(ship9);
+		world1.addWorldObject(ship10);
+		Vector2D newPos1 = new Vector2D(1080, 200);
+		Vector2D newPos2 = new Vector2D(1320, 200);
+		world1.evolve(3, null);
+		assertEquals(newPos1.getXComponent(), ship9.getPosition().getXComponent(), EPSILON);
+		assertEquals(newPos1.getYComponent(), ship9.getPosition().getYComponent(), EPSILON);
+		assertEquals(newPos2.getXComponent(), ship10.getPosition().getXComponent(), EPSILON);
+		assertEquals(newPos2.getYComponent(), ship10.getPosition().getYComponent(), EPSILON);
+	}
+	
+	@Test
+	public final void evolve_Death(){
+		world1.addWorldObject(ship9);
+		world1.addWorldObject(bullet4);
+		world1.evolve(3, null);
+		assertFalse(world1.getAllWorldObjects().contains(ship9));
+		assertFalse(world1.getAllWorldObjects().contains(bullet4));
 	}
 	
 	
