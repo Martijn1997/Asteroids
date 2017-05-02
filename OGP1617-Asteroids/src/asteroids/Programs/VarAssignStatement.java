c17ce77592a8618134264e29048170bc76442949
Martijn1997 <martijnsauwens@hotmail.com>
2017-05-02 20:18:00 +0200

    to merge or not to merge

--- OGP1617-Asteroids/src/asteroids/Programs/VarAssignStatement.java
+++ OGP1617-Asteroids/src/asteroids/Programs/VarAssignStatement.java
@@ -1,5 +1,8 @@
 package asteroids.Programs;
 
+/**
+ * Class to create a variable assignment statement
+ */
 public class VarAssignStatement<T> extends Statement implements NormalStatement{
 	
 	public VarAssignStatement(Program program, Expression<?,T> expression, String varName){
@@ -12,17 +15,23 @@ public class VarAssignStatement<T> extends Statement implements NormalStatement{
 		this.getProgram().addGlobalVariable(var.getName(), var);
 	}
 	
+	/**
+	 * evaluates the value of the variable
+	 */
 	public T getValue(){
 		return variable.evaluate();
 	}
 	
+	/**
+	 * Setter for the variable
+	 */
 	private void setVariableExpression(Expression<?,T> expression, String varName){
+		// use the outcome of the expression to create a new variable
 		this.variable = new VariableExpression<T>(varName, expression.evaluate());
 	}
 	
 	/**
 	 * basic getter for the variable of the varAssignment
-	 * @return
 	 */
 	public VariableExpression<T> getVariable(){
 		return this.variable;
